3b37a357413ae9b1cdd4590276c9a36a479712f5
MaxK0 <maxkoryakov1@gmail.com@>
2023-08-01 12:12:32 +0500

    Initial view created

--- FitnessApp.CMD/Program.cs
+++ FitnessApp.CMD/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using FitnessApp.BL.Controller;
+
+Console.WriteLine("Вас приветствует приложение Fitness");
+
+Console.Write("Введите имя пользователя: ");
+var name = Console.ReadLine();
+
+Console.Write("Введите пол: ");
+var gender = Console.ReadLine();
+
+Console.Write("Введите дату рождения: ");
+var birthDay = DateTime.Parse(Console.ReadLine());
+
+Console.Write("Введите вес: ");
+var weight = double.Parse(Console.ReadLine());
+
+Console.Write("Введите рост: ");
+var height = double.Parse(Console.ReadLine());
+
+var userController = new UserController(name, gender, birthDay, weight, height);
+userController.SaveAsync();
+
+//TODO: Сделать проверки
+//Есть идея, чтобы сделать идентификацию по телефону или почте. Если пользователь вводит уже существующий идентификатор и пароль, то заходится в приложение.
+//Ещё было бы классно сделать подтверждение по телефону без пароля.
