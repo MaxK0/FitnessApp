7d76abc1365480be2e097e183bb31d051ef410fc
MaxK0 <maxkoryakov1@gmail.com@>
2023-08-01 12:10:57 +0500

    Initial implementation of user save and download created

---
+++ FitnessApp.BL/Controller/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FitnessApp.BL.Model;
+using System.Text.Json;
+
+namespace FitnessApp.BL.Controller
+{
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Пользователь.
+        /// </summary>
+        public User User { get; set; }
+        /// <summary>
+        /// Создание нового контроллера пользователя.
+        /// </summary>        
+        public UserController(string userName, string genderName, DateTime birthDay, double weight, double height)
+        {
+            //TODO: Проверка
+
+            var gender = new Gender(genderName);
+            User = new User(userName, gender, birthDay, weight, height);
+        }
+        public UserController()
+        {
+            using (var fs = new FileStream("users.json", FileMode.OpenOrCreate))
+            {
+                if (JsonSerializer.Deserialize<User>(fs) is User user)
+                {
+                    User = user;
+                }
+                // TODO: Что делать, если пользователя не прочитали?
+            }
+        }
+        /// <summary>
+        /// Сохранить данные пользователя.
+        /// </summary>
+        public async void SaveAsync()
+        {
+            using (var fs = new FileStream("users.json", FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync<User>(fs, User);
+            } 
+        }
+    }
+}
